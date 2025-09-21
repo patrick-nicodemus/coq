d5906d032d907324811762484ba178a8b2cb8be8
Patrick Nicodemus <patrick.nicodemus5@gmail.com>
2025-09-21 00:55:13 -0400

    readme

--- library/nametab.ml
+++ library/nametab.ml
@@ -109,7 +109,7 @@ let stdlib_id = Id.of_string "Stdlib"
 
 let warn_deprecated_dirpath_Coq =
   CWarnings.create ~name:"deprecated-dirpath-Coq"
-    ~category:Deprecation.Version.v8_21
+    ~category:Deprecation.Version.v9_0
     (fun (l, id) ->
       let l' = List.rev (stdlib_id :: List.tl (List.rev l)) in
       Pp.(DirPath.(print (make l)) ++ str "." ++ Id.print id ++ spc ()
